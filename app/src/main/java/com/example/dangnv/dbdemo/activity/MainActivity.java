b1992d6d068d284f3afcf1bd2d91ab9b37b343ff
NamCF <NamCF@NamCF-PC>
2015-09-20 12:10:06 +0700

    Doi bung qua

--- app/src/main/java/com/example/dangnv/dbdemo/activity/MainActivity.java
+++ app/src/main/java/com/example/dangnv/dbdemo/activity/MainActivity.java
@@ -13,7 +13,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        
+        // Doi bung qua :(((
+        
 // tuan nguyen ptit
     }
 
