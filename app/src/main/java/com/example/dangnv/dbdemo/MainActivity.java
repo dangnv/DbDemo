7d1f356fc9ac989fffde6bfb239689b0046f284d
HoangAnhTuan <HoangAnhTuan@Adminpc>
2015-09-20 11:49:39 +0700

    make hoang anh tuan

--- app/src/main/java/com/example/dangnv/dbdemo/MainActivity.java
+++ app/src/main/java/com/example/dangnv/dbdemo/MainActivity.java
@@ -1,19 +1,16 @@
 package com.example.dangnv.dbdemo;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    DatabaseHelper databaseHelper;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        databaseHelper = new DatabaseHelper(this);
     }
 
     @Override
@@ -29,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
+        //hoang anh tuan
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
